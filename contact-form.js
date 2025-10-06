8001fc2e6fee1c46c3f0288e64676b0358b64622
ABDULRAZAQ SHUAIBU <saomeiza341@gmail.com>
2025-10-06 14:48:54 +0100

    updated all files for custom success and error/warning messages

--- contact-form.js
+++ contact-form.js
@@ -1,4 +1,7 @@
-document.getElementById("contactForm").addEventListener('submit', (e) =>{
+const form = document.getElementById("contactForm");
+const messageBox = document.getElementById("formMessage");
+
+form.addEventListener('submit', (e) =>{
     e.preventDefault();
 
     const name = document.getElementById("name").value.trim();
@@ -7,21 +10,31 @@ document.getElementById("contactForm").addEventListener('submit', (e) =>{
     const message = document.getElementById("message").value.trim();
 
     if(!name || !email || !subject || !message) {
-        alert("All fiels are required");
+        showMessage("All fiels are required", "error");
         return;
     };
 
     if(name.length===2){
-        alert("Invalid name");
+        showMessage("Invalid name", "error");
         return;
     }
 
     const emailPattern = /^[a-zA-Z0-9.-_]+@[a-zA-Z.-]+\.[a-zA-Z]{2,}$/;
     if(!email.match(emailPattern)) {
-        alert("Enter a valid email address");
+        showMessage("Enter a valid email address", "error");
         return;
     };
 
-    alert("Message sent successfully");
+    showMessage("Message sent successfully", "success");
     e.target.reset(); 
+
+    function showMessage (text, type) {
+        messageBox.textContent = text;
+        messageBox.className = type;
+        messageBox.classList.toggle("show");
+
+        setTimeout(()=>{
+            messageBox.style.opacity=0;
+        }, 4000)
+    }
 })
